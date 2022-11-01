084ad9dd7ff1881f6231a55d8c8d94d73dfb3753
JuTbn <jtoubon96@gmail.com>
2022-11-01 23:48:36 +0100

    make seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Order;
+use App\Models\Product;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +18,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()
+            ->count(10)
+            ->has(
+                Order::factory()
+                    ->count(3)
+                    ->hasAttached(
+                        Product::factory()->count(5),
+                        ['total_price' => rand(100, 500), 'total_quantity' => rand(1, 3)]
+                    )
+            )
+            ->create();
     }
 }
