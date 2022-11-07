dc724e2ec75abb24c535b2f404e7f4a1b475ac94
JuTbn <jtoubon96@gmail.com>
2022-11-07 23:33:49 +0100

    tailwind template

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import { create } from 'lodash';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const app = createApp();
+app.mount('#app');
